69e2e82161dc198d6dc9ef9efa5acadc5bdebdfd
Adrian Grzemski <adrian.grzemski@gmail.com>
2018-11-06 13:48:17 +0100

    New classes for reading GFF Files

--- include/agizmo/files.hpp
+++ include/agizmo/files.hpp
@@ -11,6 +11,7 @@ namespace AGizmo::Files {
 
 using std::getline;
 using std::ifstream;
+using std::ostream;
 using std::string;
 using std::string_view;
 using opt_str = std::optional<string>;
@@ -18,31 +19,39 @@ using std::nullopt;
 using runerror = std::runtime_error;
 
 // inline void open_file(const string &file_name, ifstream &stream) {
-inline void open_file(string_view file_name, ifstream& stream) {
+inline void open_file(string_view file_name, ifstream &stream) {
   stream = ifstream(file_name.data());
   if (!stream.is_open())
     throw runerror{"Can't open '" + string(file_name.data()) + "'\n"};
 }
 
 class FileReader {
- private:
+private:
   ifstream input;
-  string line{};
+  string file_name, line{};
 
- public:
+public:
   FileReader() = delete;
 
-  FileReader(string_view file_name) { open_file(file_name, input); }
+  FileReader(string_view file_name) : file_name{file_name} {
+    open_file(this->file_name, input);
+  }
 
   ~FileReader() { input.close(); }
 
-  string getLine() { return line; }
+  string getLine() const { return line; }
+  string str() const { return getLine(); }
 
-  bool readLine(const string& skip = {}) {
+  friend std::ostream &operator<<(ostream &stream, const FileReader &reader) {
+    return stream << reader.str();
+  }
+
+  bool readLine(const string &skip = {}) {
     if (skip.empty())
       getline(input, line);
     else
-      while (getline(input, line) && line.find_first_of(skip) == 0) continue;
+      while (getline(input, line) && line.find_first_of(skip) == 0)
+        continue;
 
     return input.good();
   }
@@ -50,12 +59,13 @@ class FileReader {
   bool readLine(int skip) {
     for (int i = 0; i < skip; ++i) {
       getline(input, line);
-      if (!input) break;
+      if (!input)
+        break;
     }
     return input.good();
   }
 
-  bool readLineInto(string& external, const string& skip = {}) {
+  bool readLineInto(string &external, const string &skip = {}) {
     if (skip.empty())
       getline(input, external);
     else
@@ -64,15 +74,16 @@ class FileReader {
     return input.good();
   }
 
-  bool readLineInto(string& external, int skip) {
+  bool readLineInto(string &external, int skip) {
     for (int i = 0; i < skip; ++i) {
       getline(input, external);
-      if (!input) break;
+      if (!input)
+        break;
     }
     return input.good();
   }
 
-  opt_str operator()(const string& skip = {}) {
+  opt_str operator()(const string &skip = {}) {
     if (readLine(skip))
       return line;
     else
@@ -80,4 +91,4 @@ class FileReader {
   }
 };
 
-}  // namespace AGizmo::Files
+} // namespace AGizmo::Files
