b6a1014ec1abb8bee2bd1f25ed03c83af821ab66
Adrian Grzemski <adrian.grzemski@gmail.com>
2018-12-04 01:05:24 +0100

    Args expanded

--- include/agizmo/args.hpp
+++ include/agizmo/args.hpp
@@ -1,22 +1,37 @@
 #pragma once
 
-#include <iostream>
 #include <optional>
+#include <stdexcept>
 #include <string>
 #include <vector>
 
 namespace AGizmo::Args {
 
-using std::cerr;
 using std::optional;
+using std::runtime_error;
 using std::string;
 using std::vector;
 
-struct Flag {
-  optional<string> name{};
-  string value{};
+class Flag {
+ private:
+  string name{};
+  optional<string> value{};
 
+ public:
   Flag() = default;
+  Flag(const string& name) {
+    if (name.empty()) throw runtime_error("Name of the flag cannot be empty!");
+    this->name = name.substr(name.find_first_not_of('-'));
+  }
+  Flag(const string& name, const string& value) : Flag(name) {
+    this->value = value;
+  }
+
+  bool hasValue() const { return value.has_value(); }
+  bool isEmpty() const { return !hasValue(); }
+  auto getName() const { return name; }
+  auto getValue() const { return value; }
+  auto getValue(const string& backup) const { return value.value_or(backup); }
 };
 
 class Arguments {
@@ -27,15 +42,35 @@ class Arguments {
   Arguments() = delete;
   Arguments(int argc, char* argv[]) {
     string name{};
-    for (int i = 1; i <= argc; ++i) {
-      if (name.empty()) {
-        string temp = argv[i];
-      } else
-        args.emplace_back(name, argv[i]);
+    for (int i = 1; i < argc; ++i) {
+      string temp = argv[i];
 
-      cerr << argv[i] << "\n";
+      if (name.empty()) {
+        if (temp.front() == '-')
+          name = temp;
+        else
+          throw runtime_error{"Missing flag before '" + temp + "'"};
+      } else {
+        if (temp.front() == '-') {
+          args.emplace_back(name);
+          name = temp;
+        } else {
+          args.emplace_back(name, temp);
+          name = {};
+        }
+      }
     }
+
+    if (!name.empty()) args.emplace_back(name);
   }
+
+  auto begin() const { return this->args.begin(); }
+  auto cbegin() const { return this->args.cbegin(); }
+  auto end() const { return this->args.end(); }
+  auto cend() const { return this->args.cend(); }
+
+  auto size() const { args.size(); }
+  auto empty() const { args.empty(); }
 };
 
 }  // namespace AGizmo::Args
