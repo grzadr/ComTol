1360a1d0591b84afe0e9e2c83ca8766c9cc42703
Adrian Grzemski <adrian.grzemski@gmail.com>
2018-12-10 15:53:22 +0100

    Added positional arguments

--- include/agizmo/args.hpp
+++ include/agizmo/args.hpp
@@ -3,6 +3,7 @@
 #include <optional>
 #include <stdexcept>
 #include <string>
+#include <variant>
 #include <vector>
 
 namespace AGizmo::Args {
@@ -15,21 +16,26 @@ using std::vector;
 class Flag {
  private:
   string name{};
+  int position{0};
   optional<string> value{};
 
  public:
-  Flag() = default;
-  Flag(const string& name) {
+  Flag() = delete;
+  Flag(const string& name, const int position) : position{position} {
     if (name.empty()) throw runtime_error("Name of the flag cannot be empty!");
     this->name = name.substr(name.find_first_not_of('-'));
   }
-  Flag(const string& name, const string& value) : Flag(name) {
+  Flag(const string& name, const int position, const string& value)
+      : Flag(name, position) {
     this->value = value;
   }
+  Flag(const int position, const string& value)
+      : position{position}, value{value} {}
 
   bool hasValue() const { return value.has_value(); }
   bool isEmpty() const { return !hasValue(); }
   auto getName() const { return name; }
+  auto getPosition() const { return position; }
   auto getValue() const { return value; }
   auto getValue(const string& backup) const { return value.value_or(backup); }
 };
@@ -42,14 +48,17 @@ class Arguments {
   Arguments() = delete;
   Arguments(int argc, char* argv[]) {
     string name{};
+
     for (int i = 1; i < argc; ++i) {
       string temp = argv[i];
 
       if (name.empty()) {
         if (temp.front() == '-')
           name = temp;
-        else
-          throw runtime_error{"Missing flag before '" + temp + "'"};
+        else {
+          args.emplace_back(temp, i);
+        }
+        //          throw runtime_error{"Missing flag before '" + temp + "'"};
       } else {
         if (temp.front() == '-') {
           args.emplace_back(name);
