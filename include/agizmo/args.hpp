a3656010011a5d8b057295624537645feafda81e
Adrian Grzemski <adrian.grzemski@gmail.com>
2018-12-17 15:41:00 +0100

    Working on args

--- include/agizmo/args.hpp
+++ include/agizmo/args.hpp
@@ -8,6 +8,7 @@
 
 namespace AGizmo::Args {
 
+using std::nullopt;
 using std::optional;
 using std::runtime_error;
 using std::string;
@@ -15,23 +16,25 @@ using std::vector;
 
 class Flag {
  private:
+  static inline const string store_true{"_true"};
+  static inline const string store_false{"_false"};
+
   string name{};
+  char alt_name{};
   optional<string> value{};
+  int position = 0;
 
  public:
   Flag() = delete;
-  Flag(const string& name) {
-    if (name.empty()) throw runtime_error("Name of the flag cannot be empty!");
-    this->name = name.substr(name.find_first_not_of('-'));
-  }
-  Flag(const string& name, const string& value) : Flag(name) {
-    this->value = value;
-  }
+  Flag(const string& name, char alt_name = 0,
+       optional<string> def_value = nullopt, int position = 0)
+      : name{name}, alt_name{alt_name}, value{def_value}, position{position} {}
 
   bool hasValue() const { return value.has_value(); }
   bool isEmpty() const { return !hasValue(); }
   bool isPositional() const { return name.front() == '_'; }
-  auto getName() const { return name; }
+  auto getName() const noexcept { return name; }
+  auto getAltName() const noexcept { return alt_name; }
   auto getPosition() const { return std::stoi(name.substr(1)); }
   auto getValue() const { return value; }
   auto getValue(const string& backup) const { return value.value_or(backup); }
@@ -42,20 +45,49 @@ class Arguments {
   vector<Flag> args{};
 
  public:
-  Arguments() = delete;
-  Arguments(int argc, char* argv[]) {
+  Arguments() = default;
+  //  Arguments(int argc, char* argv[]) {
+  //    string name{};
+  //    int position = 0;
+  //    for (int i = 1; i < argc; ++i) {
+  //      string temp = argv[i];
+
+  //      if (name.empty()) {
+  //        if (temp.front() == '-')
+  //          name = temp;
+  //        else
+  //          args.emplace_back(temp, i);
+  //      } else {
+  //        if (temp.front() == '-') {
+  //          args.emplace_back(name);
+  //          name = temp;
+  //        } else {
+  //          args.emplace_back(name, temp);
+  //          name = {};
+  //        }
+  //      }
+  //    }
+
+  //    if (!name.empty()) args.emplace_back(name);
+  //  }
+
+  void addArgument(const string& name, char alt_name = 0, int position = 0) {
+    addArgument(name, alt_name, nullopt, position);
+  }
+
+  void addArgument(const string& name, char alt_name = 0,
+                   optional<string> def_value = nullopt, int position = 0) {}
+
+  void parse(int argc, char* argv[]) {
     string name{};
-    int position = 0;
     for (int i = 1; i < argc; ++i) {
       string temp = argv[i];
 
       if (name.empty()) {
         if (temp.front() == '-')
           name = temp;
-        else {
+        else
           args.emplace_back(temp, i);
-        }
-        //          throw runtime_error{"Missing flag before '" + temp + "'"};
       } else {
         if (temp.front() == '-') {
           args.emplace_back(name);
