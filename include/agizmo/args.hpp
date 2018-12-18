e4d01fd5f1f0a09bd8fb90ccca67e1a53fc9f16e
Adrian Grzemski <adrian.grzemski@gmail.com>
2018-12-18 14:50:13 +0100

    Added help field

--- include/agizmo/args.hpp
+++ include/agizmo/args.hpp
@@ -20,15 +20,20 @@ class Flag {
   static inline const string store_false{"_false"};
 
   string name{};
+  string help{};
   char alt_name{};
   optional<string> value{};
   int position = 0;
 
  public:
   Flag() = delete;
-  Flag(const string& name, char alt_name = 0,
+  Flag(const string& name, const string& help, char alt_name = 0,
        optional<string> def_value = nullopt, int position = 0)
-      : name{name}, alt_name{alt_name}, value{def_value}, position{position} {}
+      : name{name},
+        help{help},
+        alt_name{alt_name},
+        value{def_value},
+        position{position} {}
 
   bool hasValue() const { return value.has_value(); }
   bool isEmpty() const { return !hasValue(); }
@@ -46,37 +51,16 @@ class Arguments {
 
  public:
   Arguments() = default;
-  //  Arguments(int argc, char* argv[]) {
-  //    string name{};
-  //    int position = 0;
-  //    for (int i = 1; i < argc; ++i) {
-  //      string temp = argv[i];
 
-  //      if (name.empty()) {
-  //        if (temp.front() == '-')
-  //          name = temp;
-  //        else
-  //          args.emplace_back(temp, i);
-  //      } else {
-  //        if (temp.front() == '-') {
-  //          args.emplace_back(name);
-  //          name = temp;
-  //        } else {
-  //          args.emplace_back(name, temp);
-  //          name = {};
-  //        }
-  //      }
-  //    }
-
-  //    if (!name.empty()) args.emplace_back(name);
-  //  }
-
-  void addArgument(const string& name, char alt_name = 0, int position = 0) {
-    addArgument(name, alt_name, nullopt, position);
+  void addArgument(const string& name, const string& help, char alt_name = 0,
+                   int position = 0) {
+    addArgument(name, help, alt_name, nullopt, position);
   }
 
-  void addArgument(const string& name, char alt_name = 0,
-                   optional<string> def_value = nullopt, int position = 0) {}
+  void addArgument(const string& name, const string& help, char alt_name = 0,
+                   optional<string> def_value = nullopt, int position = 0) {
+    args.emplace_back(name, help, alt_name, def_value, position);
+  }
 
   void parse(int argc, char* argv[]) {
     string name{};
