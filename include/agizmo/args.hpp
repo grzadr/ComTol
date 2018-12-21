5ccbace847b4e1ca919f687e648d9739f18623c8
Adrian Grzemski <adrian.grzemski@gmail.com>
2018-12-21 13:39:00 +0100

    Moving forward to args library

--- include/agizmo/args.hpp
+++ include/agizmo/args.hpp
@@ -3,6 +3,7 @@
 #include <optional>
 #include <stdexcept>
 #include <string>
+#include <unordered_map>
 #include <variant>
 #include <vector>
 
@@ -19,71 +20,82 @@ class Flag {
   static inline const string store_true{"_true"};
   static inline const string store_false{"_false"};
 
-  string name{};
-  string help{};
-  char alt_name{};
-  optional<string> value{};
-  int position = 0;
+  int position;
+  string name;
+  string help;
+  char alt_name;
+  optional<string> value;
 
  public:
   Flag() = delete;
-  Flag(const string& name, const string& help, char alt_name = 0,
-       optional<string> def_value = nullopt, int position = 0)
-      : name{name},
+  Flag(int position, const string& name, const string& help, char alt_name,
+       optional<string> def_value)
+      : position{position},
+        name{name},
         help{help},
         alt_name{alt_name},
-        value{def_value},
-        position{position} {}
+        value{def_value} {}
 
   bool hasValue() const { return value.has_value(); }
   bool isEmpty() const { return !hasValue(); }
-  bool isPositional() const { return name.front() == '_'; }
+  bool isPositional() const { return position > 0; }
   auto getName() const noexcept { return name; }
   auto getAltName() const noexcept { return alt_name; }
-  auto getPosition() const { return std::stoi(name.substr(1)); }
+  auto getPosition() const { return position; }
   auto getValue() const { return value; }
   auto getValue(const string& backup) const { return value.value_or(backup); }
 };
 
 class Arguments {
  private:
-  vector<Flag> args{};
+  using alt_map = std::unordered_map<char, string>;
+  using args_map = std::unordered_map<string, Flag>;
+
+  args_map args{};
+
+  void addArgument(int position, const string& name, const string& help,
+                   char alt_name, optional<string> def_value) {
+    if (name.empty()) throw runtime_error{"Argument name cannot be empty!"};
+    Flag temp_arg{position, name, help, alt_name, def_value};
+    args.emplace(name, std::move(temp_arg));
+  }
 
  public:
   Arguments() = default;
 
-  void addArgument(const string& name, const string& help, char alt_name = 0,
-                   int position = 0) {
-    addArgument(name, help, alt_name, nullopt, position);
+  void addArgument(int position, const string& name, const string& help) {
+    if (position < 1)
+      throw runtime_error{"Argument position must be greater than 0"};
+    addArgument(position, name, help, 0, nullopt);
   }
 
-  void addArgument(const string& name, const string& help, char alt_name = 0,
-                   optional<string> def_value = nullopt, int position = 0) {
-    args.emplace_back(name, help, alt_name, def_value, position);
+  void addArgument(const string& name, const string& help, char alt_name,
+                   optional<string> def_value = nullopt) {
+    addArgument(0, name, help, alt_name, def_value);
   }
 
   void parse(int argc, char* argv[]) {
-    string name{};
-    for (int i = 1; i < argc; ++i) {
-      string temp = argv[i];
+    //    string name{};
+    //    for (int i = 1; i < argc; ++i) {
+    //      string temp = argv[i];
 
-      if (name.empty()) {
-        if (temp.front() == '-')
-          name = temp;
-        else
-          args.emplace_back(temp, i);
-      } else {
-        if (temp.front() == '-') {
-          args.emplace_back(name);
-          name = temp;
-        } else {
-          args.emplace_back(name, temp);
-          name = {};
-        }
-      }
-    }
+    //      if (name.empty()) {
+    //        if (temp.front() == '-')
+    //          name = temp;
+    //        else
+    //          args.emplace_back(temp, i);
+    //      } else {
+    //        if (temp.front() == '-') {
+    //          args.emplace_back(name);
+    //          name = temp;
+    //        } else {
+    //          args.emplace_back(name, temp);
+    //          name = {};
+    //        }
+    //      }
+    //    }
 
-    if (!name.empty()) args.emplace_back(name);
+    //    if (!name.empty()) args.emplace_back(name);
   }
 
   auto begin() const { return this->args.begin(); }
