f55b670b74634822628bacf728d21855d1dc1cdc
Adrian Grzemski <Wmj3dp#w>
2019-01-25 13:37:24 +0100

    Small adjustments

--- include/agizmo/args.hpp
+++ include/agizmo/args.hpp
@@ -25,21 +25,21 @@ enum class ValueType { Bool = 1, Integer = 2, String = 3, Double = 4 };
 
 inline string str_value_type(const ValueType &value_type) {
   switch (value_type) {
-    case ValueType::Bool:
-      return "Bool";
-    case ValueType::Integer:
-      return "Integer";
-    case ValueType::String:
-      return "String";
-    case ValueType::Double:
-      return "Double";
-    default:
-      throw runtime_error{"Unknown"};
+  case ValueType::Bool:
+    return "Bool";
+  case ValueType::Integer:
+    return "Integer";
+  case ValueType::String:
+    return "String";
+  case ValueType::Double:
+    return "Double";
+  default:
+    throw runtime_error{"Unknown"};
   }
 }
 
 class Flag {
- private:
+private:
   int position;
   string name;
   string help;
@@ -47,16 +47,12 @@ class Flag {
   char alt_name;
   optional<string> value;
 
- public:
+public:
   Flag() = delete;
   Flag(int position, const string &name, const string &help,
        ValueType value_type, char alt_name, optional<string> default_val)
-      : position{position},
-        name{name},
-        help{help},
-        value_type{value_type},
-        alt_name{alt_name},
-        value{default_val} {}
+      : position{position}, name{name}, help{help},
+        value_type{value_type}, alt_name{alt_name}, value{default_val} {}
 
   friend bool operator<(const Flag &lhs, const Flag &rhs) {
     if (lhs.position == 0)
@@ -89,11 +85,12 @@ class Flag {
       output << "--";
     output << getName();
 
-    if (alt_name) output << "/" << alt_name;
+    if (alt_name)
+      output << "/" << alt_name;
 
     output << ":" << str_value_type(getValueType()) << " -> ";
     if (isLogical())
-      output << getValue("Off");
+      output << (hasValue() ? "On" : "Off");
     else
       output << getValue("None");
 
@@ -106,7 +103,7 @@ class Flag {
 };
 
 class Arguments {
- private:
+private:
   //  using alt_map = std::unordered_map<char, string>;
   //  using args_map = std::unordered_map<string, Flag>;
 
@@ -115,12 +112,13 @@ class Arguments {
   void addArgument(int position, const string &name, const string &help,
                    const ValueType &value_type, char alt_name,
                    const optional<string> &def_value) {
-    if (name.empty()) throw runtime_error{"Argument name cannot be empty!"};
+    if (name.empty())
+      throw runtime_error{"Argument name cannot be empty!"};
     args.emplace_back(position, name, help, value_type, alt_name, def_value);
     std::stable_sort(args.begin(), args.end());
   }
 
- public:
+public:
   Arguments() = default;
 
   void addArgument(int position, const string &name, const string &help,
@@ -149,7 +147,8 @@ class Arguments {
 
   auto &getArg(const string &name) {
     for (auto &ele : args) {
-      if (ele.getName() == name) return ele;
+      if (ele.getName() == name)
+        return ele;
     }
 
     throw runtime_error("Argument not found:" + name);
@@ -157,7 +156,8 @@ class Arguments {
 
   auto &getArg(const int position) {
     for (auto &ele : args) {
-      if (auto arg_pos = ele.getPosition(); arg_pos == position) return ele;
+      if (auto arg_pos = ele.getPosition(); arg_pos == position)
+        return ele;
     }
 
     throw runtime_error("Positional argument not found:" + to_string(position));
@@ -209,9 +209,8 @@ class Arguments {
           //        for (const auto &ele : temp) {
           //            if (auto arg_name = get)
           //        }
-          throw runtime_error{
-              "Multiple argument with '-' not supported. "
-              "Contact lazy developer."};
+          throw runtime_error{"Multiple argument with '-' not supported. "
+                              "Contact lazy developer."};
         }
       } else
         this->getArg(++last_poz).setValue(argv[i]);
@@ -223,26 +222,7 @@ class Arguments {
       if (arg.isPositional() && arg.isEmpty())
         throw runtime_error{"Positional argument not set -> " + arg.str()};
     }
-    //      string temp = argv[i];
-
-    //      if (name.empty()) {
-    //        if (temp.front() == '-')
-    //          name = temp;
-    //        else
-    //          args.emplace_back(temp, i);
-    //      } else {
-    //        if (temp.front() == '-') {
-    //          args.emplace_back(name);
-    //          name = temp;
-    //        } else {
-    //          args.emplace_back(name, temp);
-    //          name = {};
-    //        }
-    //      }
-    //    }
-
-    //    if (!name.empty()) args.emplace_back(name);
   }
 };
 
-}  // namespace AGizmo::Args
+} // namespace AGizmo::Args
