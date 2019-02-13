cf976b542388c6be121d953e657739667c97a856
Adrian Grzemski <adrian.grzemski@gmail.com>
2019-02-13 21:18:53 +0100

    Added Multiple arguments

--- include/agizmo/args.hpp
+++ include/agizmo/args.hpp
@@ -15,18 +15,18 @@ namespace AGizmo::Args {
 
 using std::nullopt;
 using std::optional;
-using std::runtime_error;
+using runerror = std::runtime_error;
 using std::string;
 using std::to_string;
 using std::vector;
 using sstream = std::stringstream;
 
-enum class ValueType { Bool = 1, Single = 2, Multiple = 3 };
+enum class ValueType { Switch = 1, Single = 2, Multiple = 3 };
 
 inline string str_value_type(const ValueType &value_type) {
   switch (value_type) {
-  case ValueType::Bool:
-    return "Bool";
+  case ValueType::Switch:
+    return "Switch";
   case ValueType::Single:
     return "Single";
   case ValueType::Multiple:
@@ -42,6 +42,7 @@ private:
   string name;
   string help;
   ValueType value_type;
+  int value_count;
   char alt_name;
   optional<string> value;
   bool obligatory;
@@ -67,14 +68,24 @@ public:
   bool isSet() const { return hasValue(); }
   bool isEmpty() const { return !hasValue(); }
   bool isPositional() const { return position > 0; }
-  bool isLogical() const { return value_type == ValueType::Bool; }
+  bool isSwitch() const { return value_type == ValueType::Switch; }
+  bool isMultiple() const {}
   bool isObligatory() const { return obligatory; }
+  void makeObligatory() { obligatory = true; }
+  void makeOptional() { obligatory = false; }
   auto getName() const noexcept { return name; }
   auto getAltName() const noexcept { return alt_name; }
   auto getPosition() const { return position; }
+  auto makeMultiple() { this->value_type = ValueType::Multiple; }
+  auto makeSingle() { this->value_type = ValueType::Single; }
   auto getValueType() const { return value_type; }
   void reset() { value = nullopt; }
-  auto getValue() const { return value; }
+  auto getValue() const {
+    if (this->isSet())
+      return *value;
+    else
+      throw runerror{"Argument " + name + " is not set!"};
+  }
   auto getValue(const string &backup) const { return value.value_or(backup); }
   auto setValue(const string &value) { this->value = value; }
 
@@ -90,7 +101,7 @@ public:
       output << "/" << alt_name;
 
     output << ":" << str_value_type(getValueType()) << " -> ";
-    if (isLogical())
+    if (isSwitch())
       output << (hasValue() ? "On" : "Off");
     else
       output << getValue("None");
@@ -115,27 +126,34 @@ private:
       throw runtime_error{"Argument name cannot be empty!"};
     args.emplace_back(position, name, help, value_type, alt_name, def_value,
                       obligatory);
-    std::stable_sort(args.begin(), args.end());
   }
 
 public:
   Arguments() = default;
 
-  void addArgument(const string &name, const string &help, bool obligatory) {
+  void addPositional(const string &name, const string &help,
+                     bool obligatory = false) {
     addArgument(++last_position, name, help, ValueType::Single, 0, nullopt,
                 obligatory);
   }
 
-  void addArgument(const string &name, const string &help,
-                   const ValueType &value_type, char alt_name,
-                   optional<string> def_value) {
-    addArgument(0, name, help, value_type, alt_name, def_value, false);
+  void addArgument(const string &name, const string &help, char alt_name,
+                   optional<string> def_value = nullopt) {
+    addArgument(0, name, help, ValueType::Single, alt_name, def_value, false);
   }
 
   void addArgument(const string &name, const string &help,
-                   const ValueType &value_type, char alt_name,
-                   bool obligatory = false) {
-    addArgument(0, name, help, value_type, alt_name, nullopt, obligatory);
+                   optional<string> def_value = nullopt) {
+    addArgument(0, name, help, ValueType::Single, 0, def_value, false);
+  }
+
+  void addObligatoryArgument(const string &name, const string &help,
+                             char alt_name = 0) {
+    addArgument(0, name, help, ValueType::Single, alt_name, nullopt, true);
+  }
+
+  void addSwitch(const string &name, const string &help, char alt_name = 0) {
+    addArgument(0, name, help, ValueType::Switch, alt_name, nullopt, false);
   }
 
   auto begin() const { return this->args.begin(); }
@@ -155,6 +173,15 @@ public:
     throw runtime_error("Argument not found:" + name);
   }
 
+  auto getArg(const string &name) const {
+    for (auto &ele : args) {
+      if (ele.getName() == name)
+        return ele;
+    }
+
+    throw runtime_error("Argument not found:" + name);
+  }
+
   auto &getArg(const int position) {
     for (auto &ele : args) {
       if (auto arg_pos = ele.getPosition(); arg_pos == position)
@@ -163,6 +190,9 @@ public:
 
     throw runtime_error("Positional argument not found:" + to_string(position));
   }
+
+  void makeObligatory(const string &name) { getArg(name).makeObligatory(); }
+  void allowMultipleValues(const string &name) { getArg(name).makeMultiple(); }
 
   optional<string> getArgName(const char alt_name) {
     for (auto &ele : args) {
@@ -173,7 +203,20 @@ public:
     return nullopt;
   }
 
+  bool isSet(const string &name) const { return getArg(name).isSet(); }
+
+  string getValue(const string &name) { return getArg(name).getValue(); }
+  string getValue(const string &name, const string &default_value) {
+    return getArg(name).getValue(default_value);
+  }
+  string getValue(int position) { return getArg(position).getValue(); }
+  string getValue(int position, const string &default_value) {
+    return getArg(position).getValue(default_value);
+  }
+
   void parse(int argc, char *argv[]) {
+    std::stable_sort(args.begin(), args.end());
+
     int last_poz = 0;
     for (int i = 1; i < argc; ++i) {
       string temp = argv[i];
@@ -186,7 +229,7 @@ public:
                             to_string(i)};
       else if (StringSearch::str_starts_with(temp, "--")) {
         auto &arg_ref = getArg(temp.substr(2));
-        if (arg_ref.isLogical())
+        if (arg_ref.isSwitch())
           arg_ref.setValue("On");
         else if (++i == argc)
           throw runtime_error{"Missing value for argument " + temp};
@@ -196,7 +239,7 @@ public:
         if (temp = temp.substr(1); temp.size() == 1) {
           if (const auto &arg_name = getArgName(temp.front())) {
             auto &arg_ref = getArg(*arg_name);
-            if (arg_ref.isLogical())
+            if (arg_ref.isSwitch())
               arg_ref.setValue("");
             else if (++i == argc)
               throw runtime_error{"Missing value for argument " + temp};
